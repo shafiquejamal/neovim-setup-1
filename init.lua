e74202132c80948ed0c5f5f1132e0262d926775a
shafiquejamal <shafique.jamal@gmail.com>
2025-01-09 15:10:42 -0500

    Don't overrite my copy buffer when deleting

--- init.lua
+++ init.lua
@@ -113,8 +113,15 @@ vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper win
 vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal left<CR>", {})
 
 -- delete without copying into register
+vim.keymap.set({ "n", "v" }, "X", '"_X')
 vim.keymap.set({ "n", "v" }, "x", '"_x')
+vim.keymap.set({ "n", "v" }, "D", '"_D')
 vim.keymap.set({ "n", "v" }, "d", '"_d')
+
+-- https://www.reddit.com/r/neovim/comments/v7s1ts/how_do_i_avoid_replacing_the_content_of_my/
+vim.keymap.set("x", "p", function()
+	return 'pgv"' .. vim.v.register .. "y"
+end, { remap = false, expr = true })
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
