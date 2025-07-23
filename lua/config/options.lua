2daabbd3979969c30f53bb7d9b7bbcb481f32e4a
GeloCraft <115651305+gelocraft@users.noreply.github.com>
2025-07-23 08:23:01 +0800

    chore: disable ignorecase option (#31)

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -36,7 +36,7 @@ opt.breakindent = true
 opt.undofile = true
 
 -- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
-opt.ignorecase = true
+opt.ignorecase = false
 opt.smartcase = true
 
 -- Decrease update time
