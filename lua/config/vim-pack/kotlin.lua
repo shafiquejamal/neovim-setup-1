510c4a3a2bed65b7f1fe15539365ce50256bf00d
Shafique Jamal <shafique.jamal@gmail.com>
2026-06-13 01:37:27 -0400

    Stylua check

--- lua/config/vim-pack/kotlin.lua
+++ lua/config/vim-pack/kotlin.lua
@@ -10,15 +10,20 @@ end
 -- Falls back to cwd if no gradlew is found upward.
 local function gradlew_cmd(task)
 	return function()
-		local root = vim.fs.root(0, { "gradlew", "settings.gradle", "settings.gradle.kts" })
-		local cmd = (root and (root .. "/gradlew") or "./gradlew") .. " " .. task
+		local root = vim.fs.root(
+			0,
+			{ "gradlew", "settings.gradle", "settings.gradle.kts" }
+		)
+		local cmd = (root and (root .. "/gradlew") or "./gradlew")
+			.. " "
+			.. task
 		vim.cmd("botright 15split | term " .. cmd)
 	end
 end
 
-keymap("<leader>gdb", gradlew_cmd("build"), "[G]ra[d]le [b]uild")
-keymap("<leader>gdt", gradlew_cmd("test"), "[G]ra[d]le [t]est")
-keymap("<leader>gdc", gradlew_cmd("clean"), "[G]ra[d]le [c]lean")
-keymap("<leader>gdr", gradlew_cmd("run"), "[G]ra[d]le [r]un")
-keymap("<leader>gdR", gradlew_cmd("runClient"), "[G]ra[d]le [R]un client")
-keymap("<leader>gdC", gradlew_cmd("clean build"), "[G]ra[d]le [C]lean build")
+keymap("<leader>gdb", gradlew_cmd "build", "[G]ra[d]le [b]uild")
+keymap("<leader>gdt", gradlew_cmd "test", "[G]ra[d]le [t]est")
+keymap("<leader>gdc", gradlew_cmd "clean", "[G]ra[d]le [c]lean")
+keymap("<leader>gdr", gradlew_cmd "run", "[G]ra[d]le [r]un")
+keymap("<leader>gdR", gradlew_cmd "runClient", "[G]ra[d]le [R]un client")
+keymap("<leader>gdC", gradlew_cmd "clean build", "[G]ra[d]le [C]lean build")
--- lua/plugins/kotlin.lua
+++ lua/plugins/kotlin.lua
@@ -4,8 +4,13 @@
 
 local function gradlew_cmd(task)
 	return function()
-		local root = vim.fs.root(0, { "gradlew", "settings.gradle", "settings.gradle.kts" })
-		local cmd = (root and (root .. "/gradlew") or "./gradlew") .. " " .. task
+		local root = vim.fs.root(
+			0,
+			{ "gradlew", "settings.gradle", "settings.gradle.kts" }
+		)
+		local cmd = (root and (root .. "/gradlew") or "./gradlew")
+			.. " "
+			.. task
 		vim.cmd("botright 15split | term " .. cmd)
 	end
 end
@@ -17,12 +22,28 @@ return {
 		"nvim-lua/plenary.nvim",
 		lazy = true, -- already a dep elsewhere; this spec only adds keys
 		keys = {
-			{ "<leader>gdb", gradlew_cmd("build"), desc = "[G]ra[d]le [b]uild" },
-			{ "<leader>gdt", gradlew_cmd("test"), desc = "[G]ra[d]le [t]est" },
-			{ "<leader>gdc", gradlew_cmd("clean"), desc = "[G]ra[d]le [c]lean" },
-			{ "<leader>gdr", gradlew_cmd("run"), desc = "[G]ra[d]le [r]un" },
-			{ "<leader>gdR", gradlew_cmd("runClient"), desc = "[G]ra[d]le [R]un client" },
-			{ "<leader>gdC", gradlew_cmd("clean build"), desc = "[G]ra[d]le [C]lean build" },
+			{
+				"<leader>gdb",
+				gradlew_cmd "build",
+				desc = "[G]ra[d]le [b]uild",
+			},
+			{ "<leader>gdt", gradlew_cmd "test", desc = "[G]ra[d]le [t]est" },
+			{
+				"<leader>gdc",
+				gradlew_cmd "clean",
+				desc = "[G]ra[d]le [c]lean",
+			},
+			{ "<leader>gdr", gradlew_cmd "run", desc = "[G]ra[d]le [r]un" },
+			{
+				"<leader>gdR",
+				gradlew_cmd "runClient",
+				desc = "[G]ra[d]le [R]un client",
+			},
+			{
+				"<leader>gdC",
+				gradlew_cmd "clean build",
+				desc = "[G]ra[d]le [C]lean build",
+			},
 		},
 	},
 }
