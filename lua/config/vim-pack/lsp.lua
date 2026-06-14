98f70c27eb63004d2a6060a8e5e49c87ea49cb2e
Shafique Jamal <shafique.jamal@gmail.com>
2026-06-14 17:09:10 -0400

    feat: use official Kotlin LSP

--- lua/config/vim-pack/lsp.lua
+++ lua/config/vim-pack/lsp.lua
@@ -10,6 +10,14 @@ vim.pack.add {
 
 require("mason").setup()
 
+local lspconfig = require("lspconfig")
+
+local function make_capabilities()
+	return require("cmp_nvim_lsp").default_capabilities(
+		vim.lsp.protocol.make_client_capabilities()
+	)
+end
+
 require("mason-tool-installer").setup {
 	run_on_start = true,
 	start_delay = 3000,
@@ -23,7 +31,6 @@ require("mason-tool-installer").setup {
 		"rust-analyzer",
 		"lua-language-server",
 		"typescript-language-server",
-		"kotlin-language-server",
 
 		-- LINTERS
 		"eslint",
@@ -59,13 +66,20 @@ local lsp_settings = {
 require("mason-lspconfig").setup {
 	handlers = {
 		function(lsp)
-			local capabilities = vim.lsp.protocol.make_client_capabilities()
-			capabilities =
-				require("cmp_nvim_lsp").default_capabilities(capabilities)
-			require("lspconfig")[lsp].setup {
-				capabilities = capabilities,
+			lspconfig[lsp].setup {
+				capabilities = make_capabilities(),
 				settings = lsp_settings[lsp] or {},
 			}
 		end,
 	},
 }
+
+
+-- Official Kotlin LSP is installed outside Mason.
+local kotlin_lsp = vim.fn.exepath "kotlin-lsp"
+if kotlin_lsp ~= "" then
+	lspconfig.kotlin_lsp.setup {
+		capabilities = make_capabilities(),
+		cmd = { kotlin_lsp, "--stdio" },
+	}
+end
